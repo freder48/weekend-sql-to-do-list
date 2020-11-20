1bee5f1a6e5002d2468104b029ec18f5f192d917
Josie Fredericksen <jfredericksen12@gmail.com>
2020-11-19 18:21:27 -0600

    Added GET route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,26 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('Hello from JQ');
+    $('#addBtn').on('click', postTask);
+    getTasks();
+}
+
+//start getTasks
+function getTasks() {
+    console.log('in getTasks');
+    // ajax call to server to get tasks
+    $.ajax({
+      method: 'GET',
+      url: '/tasks'
+    }).then(function (response) {
+      console.log(response)
+      renderTasks();
+    }).catch(function (error) {
+      console.log('Error in client.js GET', error)
+    });
+  } // end getTasks
+
+function postTask(){
+    console.log('Clicked');
     
 }
--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -1,3 +1,24 @@
 const express = require('express');
-const router = express.Router();
+const todoRouter = express.Router();
 const pool = require('../modules/pool');
+
+//GET ROUTE
+todoRouter.get('/', (req, res) => {
+    let sqlText = `SELECT * FROM "tasks" ORDER BY "status";`;
+    pool.query(sqlText)
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(error => {
+            console.log('Error in getting tasks', error);
+            res.sendStatus(500);
+        });
+})
+
+
+
+
+
+
+
+module.exports = todoRouter;
